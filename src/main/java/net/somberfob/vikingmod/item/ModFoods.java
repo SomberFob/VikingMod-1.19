421798a99d6536afd7bc387edbd127b2537907be
SomberFob <benos102@hotmail.com>
2023-04-22 17:29:05 +0100

    added- chanterelles mushroom (food)

--- src/main/java/net/somberfob/vikingmod/item/ModFoods.java
+++ src/main/java/net/somberfob/vikingmod/item/ModFoods.java
@@ -6,8 +6,8 @@ import net.minecraft.world.food.FoodProperties;
 
 public class ModFoods {
 
-    public static final FoodProperties GOLDEN_RASPBERRIES = (new FoodProperties.Builder()).nutrition(3).saturationMod(1.5F).effect(new MobEffectInstance(MobEffects.REGENERATION, 90, 1), 1.0F).alwaysEat().build();
-
+    public static final FoodProperties GOLDEN_RASPBERRIES = (new FoodProperties.Builder()).nutrition(4).saturationMod(1.5F).effect(new MobEffectInstance(MobEffects.REGENERATION, 90, 1), 1.0F).alwaysEat().fast().build();
     public static final FoodProperties RASPBERRIES = (new FoodProperties.Builder()).nutrition(2).saturationMod(0.9F).build();
 
+    public static final FoodProperties CHANTERELLES_MUSHROOM = (new FoodProperties.Builder()).nutrition(2).saturationMod(0.7F).meat().build();
 }
--- src/main/java/net/somberfob/vikingmod/item/ModItems.java
+++ src/main/java/net/somberfob/vikingmod/item/ModItems.java
@@ -38,6 +38,9 @@ public class ModItems {
     public static final RegistryObject<Item> RASPBERRIES = ITEMS.register("raspberries",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.CONSUMABLES).food(ModFoods.RASPBERRIES)));
 
+    public static final RegistryObject<Item> CHANTERELLES_MUSHROOM = ITEMS.register("chanterelles_mushroom",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.CONSUMABLES).food(ModFoods.CHANTERELLES_MUSHROOM)));
+
 
     public static final RegistryObject<Item> OIL_JAR = ITEMS.register("oil_jar",
             () -> new OilJarItem(new Item.Properties().tab(ModCreativeModeTab.BARTER_AND_CRAFTING_ITEMS).stacksTo(7)));
